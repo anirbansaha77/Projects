31eb434b9f743f5b74094050ba334a54863d8e09
anirbansaha77 <anirbansaha77@gmail.com>
2018-02-03 16:39:28 -0600

    test commit

--- interviewQuestions/creditkarma_2018_Feb/findColor.test.js
+++ interviewQuestions/creditkarma_2018_Feb/findColor.test.js
@@ -14,7 +14,7 @@ describe('Test findcolor creditkarma phoneinterview 2018 feb first round', () =>
     it("findColor('zre') should return [ 'azure' ]", () => {
         expect(findColor('zre')).deep.equal(secondResult);
     });
-
+ 
     it("findColor('uqi') should return [ 'darkturquoise', 'mediumaquamarine', 'mediumturquoise', 'paleturquoise', 'turquoise' ]", () => {
         expect(findColor('uqi')).deep.equal(thirdResult);
     });
